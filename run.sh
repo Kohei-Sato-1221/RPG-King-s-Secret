46b2a5eba21a3617488ed952020f21ad6c31b6e9
kohei.sato <night.god.moon.garsu@gmail.com>
2022-05-06 18:24:50 +0900

    起動スクリプトを追加

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+echo ''
+echo ''
+echo '起動されたChromeにてindex.htmlをドラッグ＆ドロップして開いてください。'
+echo ''
+echo ''
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --allow-file-access-from-files
